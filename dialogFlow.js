3519d3032751cbaed57335c98a866c21e275e1bc
sheansuke <suerojean@gmail.com>
2020-10-27 16:20:28 -0400

    add try catch to dialogFlow.js

--- dialogFlow.js
+++ dialogFlow.js
@@ -10,44 +10,48 @@ const uuid = require("uuid");
  */
 module.exports = {
   chatBot: async function (message) {
-    // A unique identifier for the given session
-    const sessionId = uuid.v4();
+    try {
+      // A unique identifier for the given session
+      const sessionId = uuid.v4();
 
-    // Create a new session
-    const sessionClient = new dialogflow.SessionsClient();
-    const sessionPath =  await sessionClient.sessionPath(
-      process.env.DIALOGFLOW_PROJECTID,
-      sessionId
-    );
+      // Create a new session
+      const sessionClient = new dialogflow.SessionsClient();
+      const sessionPath = await sessionClient.sessionPath(
+        process.env.DIALOGFLOW_PROJECTID,
+        sessionId
+      );
 
-    // The text query request.
-    const request = {
-      session: sessionPath,
-      queryInput: {
-        text: {
-          // The query to send to the dialogflow agent
-          text: message,
-          // The language used by the client (en-US)
-          languageCode: "es-ES",
+      // The text query request.
+      const request = {
+        session: sessionPath,
+        queryInput: {
+          text: {
+            // The query to send to the dialogflow agent
+            text: message,
+            // The language used by the client (en-US)
+            languageCode: "es-ES",
+          },
         },
-      },
-    };
+      };
 
-    // Send request and log result
-    const responses = await sessionClient.detectIntent(request);
-    console.log("Detected intent");
-    const result = responses[0].queryResult;
-    console.log(`  Query: ${result.queryText}`);
-    console.log(`  Response: ${result.fulfillmentText}`);
-    if (result.intent) {
-      console.log(`  Intent: ${result.intent.displayName}`);
-    } else {
-      console.log(`  No intent matched.`);
+      // Send request and log result
+      const responses = await sessionClient.detectIntent(request);
+      console.log("Detected intent");
+      const result = responses[0].queryResult;
+      console.log(`  Query: ${result.queryText}`);
+      console.log(`  Response: ${result.fulfillmentText}`);
+      if (result.intent) {
+        console.log(`  Intent: ${result.intent.displayName}`);
+      } else {
+        console.log(`  No intent matched.`);
+      }
+      return {
+        query: result.queryText,
+        response: result.fulfillmentText,
+        intent: result.intent.displayName,
+      };
+    } catch (error) {
+      console.log(error);
     }
-    return {
-      query: result.queryText,
-      response: result.fulfillmentText,
-      intent: result.intent.displayName,
-    };
   },
 };
