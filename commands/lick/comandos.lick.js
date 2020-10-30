1f7f5b78132a5259977f9a00cdebde9d0dfa96ce
sheansuke <suerojean@gmail.com>
2020-10-30 14:06:32 -0400

    agregado comando lick

---
+++ commands/lick/comandos.lick.js
@@ -0,0 +1,18 @@
+//IMPORT CLIENT
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+module.exports = {
+  command: "comandos.lick",
+  desciption: "Show anime lick gif or image",
+  run: async (client, message, data) => {
+    const usuario = message.mentions.members.first();
+    const embed = new Discord.MessageEmbed();
+    embed.setColor("RANDOM");
+    embed.setDescription(
+      `**${message.author.username}** lamio a **${usuario.user.username} o///o **`
+    );
+    embed.setImage(data);
+    return message.channel.send(embed);
+  },
+};
--- index.js
+++ index.js
@@ -33,6 +33,7 @@ client.on("ready", () => {
 
 // OPTIENE Y PROCESA LAS ENTRADAS DE LOS USUARIOS
 client.on("message", (msg) => {
+  console.log(msg)
   // PREFIJO BAJO EL QUE DEBE INICIAR CADA FRASE DICHA AL BOT
   const startPrefix = msg.content.startsWith(prefix);
 
